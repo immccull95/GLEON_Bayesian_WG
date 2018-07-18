c8032445e69b4dc29a989a8ee4f5593c5be8af1d
jzwart <jzwart@usgs.gov>
2018-07-18 11:08:58 -0500

    adding get data

--- RCode/NEFI/get_data.R
+++ RCode/NEFI/get_data.R
@@ -1,3 +1,17 @@
 # function for pulling data and getting it into a usable format 
 
-library(readxl)
+require(dplyr)
+
+get_data <- function(cal_time_start, cal_time_end, forecast_time_end, sites){
+  
+  data <- read.csv('Datasets/Sunapee/R Work/Level 1/All_Sites_Gloeo.csv', stringsAsFactors = F) %>%
+    as_data_frame() %>% 
+    mutate(date = lapply(date, function(date){as.Date(strsplit(date, 'T')[[1]][1]) %>% as_data_frame()}) %>%  # changing date format to date 
+    bind_rows() %>%
+    pull(value)) %>%
+    dplyr::filter(site %in% sites) 
+  
+  cal <- data %>%
+    dplyr::filter(date > as.Date(cal_time_start), date < as.Date(cal_time_end)) 
+
+}
--- RCode/NEFI/model_wrapper.R
+++ RCode/NEFI/model_wrapper.R
@@ -8,9 +8,18 @@ library(R2jags)
 cal_time_start <- '1990-01-01' 
 cal_time_end <- '2013-01-01' 
 forecast_time_end <- '2016-01-01' 
+sites <- c('1') 
 
-data <- get_data(cal_time_start, cal_time_end, forecast_time_end) # get the data
+data <- get_data(cal_time_start, cal_time_end, forecast_time_end, sites) # get the data
 
+init <- 1 
+
+
+
+j.model <- jags.model (file = textConnection(model),
+                       data = data,
+                       inits = init,
+                       n.chains = 3)
 # call the model 
 
 
