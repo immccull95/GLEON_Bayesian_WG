6b6bdee05c700e749953e85cfd2a8fbe2c9a372b
jzwart <jzwart@usgs.gov>
2018-07-19 10:37:48 -0500

    getting wtr temp

--- RCode/NEFI/get_data.R
+++ RCode/NEFI/get_data.R
@@ -4,12 +4,22 @@ require(dplyr)
 
 get_data <- function(cal_time_start, cal_time_end, forecast_time_end, sites){
   
-  data <- read.csv('Datasets/Sunapee/R Work/Level 1/All_Sites_Gloeo.csv', stringsAsFactors = F) %>%
+  gleo <- read.csv('Datasets/Sunapee/R Work/Level 1/All_Sites_Gloeo.csv', stringsAsFactors = F) %>%
     as_data_frame() %>% 
     mutate(date = lapply(date, function(date){as.Date(strsplit(date, 'T')[[1]][1]) %>% as_data_frame()}) %>%  # changing date format to date 
     bind_rows() %>%
     pull(value)) %>%
+    mutate(week = lubridate::week(date),
+           site = tolower(site)) %>%
     dplyr::filter(site %in% sites) 
+
+  
+  # temperature data 
+  wtr <- read.csv('Datasets/Sunapee/R Work/Level 1/wtr_temp_long_weekly_summary.csv', stringsAsFactors = F) %>%
+    select(-X) %>%
+    mutate(site = tolower(site)) 
+  
+  data <- left_join(gleo, wtr, by = c('week', 'year', 'site')) 
   
   cal <- data %>%
     dplyr::filter(date > as.Date(cal_time_start), date < as.Date(cal_time_end)) 
