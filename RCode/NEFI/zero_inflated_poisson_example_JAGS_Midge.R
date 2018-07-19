b7b8627bec61df55c7539119362d8462d15d05c3
Whitney Beck <wbeck1990@gmail.com>
2018-07-19 11:06:05 -0600

    New File

---
+++ RCode/NEFI/zero_inflated_poisson_example_JAGS_Midge.R
@@ -0,0 +1,66 @@
+#building zero inflated data, fitting it with a zero inflated model.
+#clear environment, load packages.----
+rm(list=ls())
+library(runjags)
+
+data=All_Sites_Gloeo
+midge=subset(All_Sites_Gloeo, site=="Midge")
+
+y=midge$coloniesperL
+x=midge$month
+
+#simulate data.----
+N <- 230
+
+#Lets build a JAGS model that can recover those true parameters.----
+
+jags.model <- "
+model{
+  for(i in 1:N){
+    #this fits the blended model to your observed data. 
+    y[i] ~ dpois(m[i])
+
+        #This blends the poisson and zero inflation models
+    m[i] <- mu[i]*b[i] + 1E-10 #adding the tiny value is important (i forget why...)
+    
+    #this is the bernoulli outcome of the zero inflation
+    b[i] ~ dbern(theta[i])
+    
+    #mu[i] is the linear combination of predictors and parameters for the poisson component of the model.
+    log(mu[i]) <- beta.pois[1] + beta.pois[2]*x[i]
+    
+    #theta[i] is the linear combination of predictors and paramters for the bernoulli component of the model.
+    logit(theta[i]) <- beta.bern[1] + beta.bern[2]*x[i]
+  }
+  
+  #setup your priors. These are flat, uninformative priors.
+  for(i in 1:N.pred){
+    beta.pois[i] ~ dnorm(0, 1E-3)
+    beta.bern[i] ~ dnorm(0, 1E-3)
+  }
+
+} #close model loop.
+"
+
+#setup JAGS data object.----
+#N.pred = 2. one predictor is the intercept, the second is x (month of year).
+
+jags.data <- list(y = y, x = x, N = 230, N.pred = 2)
+nchain=3
+
+j.model   <- jags.model(file = textConnection(jags.model),
+                        data = jags.data,
+                        n.chains = nchain)
+
+var.out   <- coda.samples (model = j.model,
+                           variable.names = c("beta.pois", "beta.bern"),
+                           n.iter = 3000)
+
+#summaryize output.
+jags.sum <- summary(var.out)
+
+#compare fitted parameter values to true parameter values.----
+#bernoulli parameters more likely to be the center of the distribution, as wel observe N binary outcomes.
+#poisson parameters will be less likely to be at the center of the distribution as few observations are non-zero.
+true <- c(beta.pois, beta.bern)
+cbind(jags.sum[,1:3],true)
