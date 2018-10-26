0048b667f7aa78869aa70e24557808101b372d8d
melofton <melofton@vt.edu>
2018-10-26 14:22:19 -0400

    action items from 05OCT18 meeting

--- RCode/NEFI/zero_inflated_poisson_example_JAGS_MEL.R
+++ RCode/NEFI/zero_inflated_poisson_example_JAGS_MEL.R
@@ -2,25 +2,30 @@
 #clear environment, load packages.----
 rm(list=ls())
 library(runjags)
+library(coda)
 
 
 #simulate data.----
-N <- 1000
+N <- 1200
 
 #lets create a covariate, say temperature because I'm guessing that matters for algae.
-x <- runif(N, 5, 25)
+x1 <- runif(N, -10, 30) #simulated air temperature
+x2 <- runif(N, 4, 25) #simulated water temperature
+
+plot(seq(1,1200,1),jags.data$x1, col = "red", type = "l", main = "Simulated Air Temp.")
+plot(seq(1,1200,1),jags.data$x2, col = "blue", type = "l", main = "simulated Water Temp.")
 
 #Lets specify a poisson distribution where the abundance depends on temeperature and an intercept.
 #we're going to exponentiate because we use a log link in the model.
 beta.pois <- c(1,0.1)
-y.pois <- exp(beta.pois[1] + x*beta.pois[2])
+y.pois <- exp(beta.pois[1] + x2*beta.pois[2])
 
 #Time to zero inflate. Zero inflation will also depend on temperature and an intercept.
 #I am simulate the probabilty as a continuous value.
 #I then send these values data through a logit transform to put it on the scale (0,1)
 #setting the intercept to be negative so this is often zero.
 beta.bern <- c(-3, 0.1)
-y.prob <- beta.bern[1] + x*beta.bern[2]
+y.prob <- beta.bern[1] + x1*beta.bern[2]
 y.prob <- boot::inv.logit(y.prob)
 
 #send through the binomial data model to get zero-one outcomes.
@@ -31,6 +36,23 @@ y.bern <- rbinom(N, 1, y.prob)
 #take that outcome, send it through the poisson data model.
 y.obs <- y.bern*y.pois
 y.obs <- rpois(N, lambda = y.bern*y.pois)
+plot(seq(1,1200,1),y.obs, type = "p", main = "Simulated Gloeo data")
+
+#set values in "winter" to NA
+y.obs[c(121:360, 481:720, 841:1080)] <- NA
+plot(seq(1,1200,1),y.obs, type = "p", main = "Simulated Gloeo data with winter")
+
+
+#set values in between "weekly sampling" to NA
+ind <- seq(1, length(y.obs), by=7)
+
+for (i in 1:length(y.obs)){
+  if(!(i %in% ind)){
+    y.obs[i] <- NA
+  }
+}
+plot(seq(1,1200,1),y.obs, type = "p", main = "Simulated Gloeo data with winter & weekly obs.")
+
 
 #Cool. We now have zero inflate data where we know all of the true parameters!
 
@@ -38,7 +60,7 @@ y.obs <- rpois(N, lambda = y.bern*y.pois)
 
 jags.model <- "
 model{
-  for(i in 1:N){
+  for(i in 2:N){
     #this fits the blended model to your observed data. 
     y[i] ~ dpois(m[i])
     
@@ -49,10 +71,12 @@ model{
     b[i] ~ dbern(theta[i])
     
     #mu[i] is the linear combination of predictors and parameters for the poisson component of the model.
-    log(mu[i]) <- beta.pois[1] + beta.pois[2]*x[i]
+    #water temp is included as a covariate for the poisson
+    log(mu[i]) <- beta.pois[1] + beta.pois[2]*x2[i] + beta.pois[3]*y[i-1]
     
     #theta[i] is the linear combination of predictors and paramters for the bernoulli component of the model.
-    logit(theta[i]) <- beta.bern[1] + beta.bern[2]*x[i]
+    #air temp is included as a covariate for the bernoulli
+    logit(theta[i]) <- beta.bern[1] + beta.bern[2]*x1[i] + beta.bern[3]*y[i-1]
   }
   
   #setup your priors. These are flat, uninformative priors.
@@ -66,31 +90,38 @@ model{
 
 #setup JAGS data object.----
 #N.pred = 2. one predictor is the intercept, the second is x ("temperature").
-jags.data <- list(y = y.obs, x = x, N = N, N.pred = 2)
+jags.data <- list(y = y.obs, x1 = x1, x2 = x2, N = N, N.pred = 3)
+
+#set up initial conditions - haven't done this yet
+
 
 #fit the jags object using runjags.----
 jags.out <- run.jags(    model = jags.model,
                           data = jags.data,
-                         adapt =  100,
-                        burnin =  500,
-                        sample = 1000,
+                         adapt =  500,
+                        burnin =  1000,
+                        sample = 10000,
                       n.chains = 3,
                        monitor = c('beta.pois','beta.bern'))
 
-#summaryize output.
-jags.sum <- summary(jags.out)
+#summarize output.
+print(jags.out)
+plot(jags.out)
+jags.out.mcmc <- as.mcmc.list(jags.out)
+summary(jags.out.mcmc)
+gelman.plot(jags.out.mcmc)
 
 #compare fitted parameter values to true parameter values.----
 #bernoulli parameters more likely to be the center of the distribution, as wel observe N binary outcomes.
 #poisson parameters will be less likely to be at the center of the distribution as few observations are non-zero.
-true <- c(beta.pois, beta.bern)
-cbind(jags.sum[,1:3],true)
-
-#plot, just for fun.
-var.mat <- do.call(rbind, jags.out$mcmc)
-par(mfrow = c(2,2))
-for(i in 1:ncol(var.mat)){
-  hist(var.mat[,i], main = colnames(var.mat)[i])
-  abline(v = true[i], lwd = 2, col= 'purple')
-}
+# true <- c(beta.pois, beta.bern)
+# cbind(jags.sum[,1:3],true)
+# 
+# #plot, just for fun.
+# var.mat <- do.call(rbind, jags.out$mcmc)
+# par(mfrow = c(2,2))
+# for(i in 1:ncol(var.mat)){
+#   hist(var.mat[,i], main = colnames(var.mat)[i])
+#   abline(v = true[i], lwd = 2, col= 'purple')
+# }
 
