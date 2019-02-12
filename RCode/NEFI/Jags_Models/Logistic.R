86bd21c44b67796e8928ada141f1149007c52347
Whitney Beck <wbeck1990@gmail.com>
2019-02-12 16:32:42 -0700

    Functioning logistic model?

--- RCode/NEFI/Jags_Models/Logistic.R
+++ RCode/NEFI/Jags_Models/Logistic.R
@@ -2,21 +2,17 @@ model{
   for(i in 1:N){
     #this fits the blended model to your observed data. 
     y[i] ~ dpois(m[i])
-    
-    m[i] <- exp(mu[i])
-    
+    m[i]<- exp(mu[i])
   }
-  
   #### Process Model
   for(i in 2:N){
-    mu[i]~dnorm(lambda[i],tau_add) 
-    lambda[i] <- log(beta[1]*m[i-1]+ beta[2]*m[i-1]^2)
+   mu[i]~dnorm(lambda[i],tau_add) 
+   log(lambda[i])<- beta[1]*mu[i-1] + beta[2]*mu[i-1]^2
     
   }
   
   #### Priors
-  mu[1] ~ dnorm(x_ic,tau_ic) 
+  mu[1]~ dnorm(x_ic, tau_ic)
   tau_add ~ dgamma(a_add,r_add)
-  beta~ dmnorm(beta.m, beta.v)
-  
+  beta ~ dmnorm(beta.m,beta.v)
 }
