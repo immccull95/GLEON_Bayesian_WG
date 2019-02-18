dbd501de095d5fbc4f31c83067f7fcad087b5f42
Whitney Beck <wbeck1990@gmail.com>
2019-02-18 10:55:33 -0700

    Added note about beta interpretations

--- RCode/NEFI/Jags_Models/Logistic.R
+++ RCode/NEFI/Jags_Models/Logistic.R
@@ -1,3 +1,6 @@
+##Note for logistic model: beta[1]= 1+r and beta[2] = -r/k
+##If we want more informative priors, we can calculate betas from component parts and put priors on r & k
+
 model{
   for(i in 1:N){
     #this fits the blended model to your observed data. 
