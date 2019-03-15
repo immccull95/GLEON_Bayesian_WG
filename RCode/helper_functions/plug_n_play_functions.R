77de18bc8d25c032749d8550afd84bc9631614ab
Whitney Beck <wbeck1990@gmail.com>
2019-03-15 15:22:59 -0600

    Adding code to create different initial conditions for each model in functions script

--- RCode/helper_functions/plug_n_play_functions.R
+++ RCode/helper_functions/plug_n_play_functions.R
@@ -1,5 +1,6 @@
 # plug and play scripts 
 # JAZ 2019-02-15
+# WB Updates
 
 
 plug_n_play_data <- function(start_date, end_date, sites, model_timestep, fill_dates){
@@ -31,74 +32,82 @@ plug_n_play_data <- function(start_date, end_date, sites, model_timestep, fill_d
 
 jags_plug_ins <- function(model_name){
 
-  #, x_ic = log(0.1), tau_ic = 100, a_add = 0.001, r_add = 0.001)
-  #JAGS Plug-ins: Add each separate model here 
-  #variable.names are variables you would like to plot for model diagnostics (e.g., excludes mu)
-  #variable.names.out are all variables you would like to monitor in the jags run 
+#JAGS Plug-ins: Add each separate model here 
+#variable.names are variables you would like to plot for model diagnostics (e.g., excludes mu)
+#variable.names.out are all variables you would like to monitor in the jags run 
+#init are a range of initial conditions for parameters in each of 3 chains 
   
 #Random Walk
   data.RandomWalk <- list(y=y, N=length(y),x_ic=log(0.1),tau_ic = 100, a_add = 0.001,r_add = 0.001)
   variable.names.RandomWalk<- c("tau_add")
   variable.namesout.RandomWalk<- c("tau_add", "mu")
-
+  init.RandomWalk <- list(list(tau_add=0.001), list(tau_add=0.1), list(tau_add=1))
+  
 #RandomWalkZip
   data.RandomWalkZip <- list(y=y, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001, alpha=1, epsilon=1)
   variable.names.RandomWalkZip<- c("tau_add", "theta")
   variable.namesout.RandomWalkZip<- c("tau_add", "mu", "theta")
-
+  init.RandomWalkZip <- list(list(tau_add=0.001, theta=0), list(tau_add=0.1, theta=0.5), list(tau_add=1, theta=1))
+  
 #DayLength  
   data.DayLength <- list(y=y, beta.m=as.vector(c(0,0,0)), beta.v=solve(diag(1E-03,3)), DL=DL, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001)
   variable.names.DayLength <- c("tau_add", "beta")
   variable.namesout.DayLength <- c("tau_add", "beta", "mu")
-
+  init.DayLength <- list(list(tau_add=0.001, beta=c(-0.5,-0.5,-0.5)), list(tau_add=0.1, beta=c(0,0,0)), list(tau_add=1, beta=c(0.5,0.5,0.5)))
+  
 #DayLengthQuad
   data.DayLengthQuad <- list(y=y, beta.m=as.vector(c(0,0,0,0)), beta.v=solve(diag(1E-03,4)), DL=DL, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001)
   variable.names.DayLengthQuad <- c("tau_add", "beta")
   variable.namesout.DayLengthQuad <- c("tau_add", "beta", "mu")
-
+  init.DayLengthQuad <- list(list(tau_add=0.001, beta=c(-0.5,-0.5,-0.5,-0.5)), list(tau_add=0.1, beta=c(0,0,0,0)), list(tau_add=1, beta=c(0.5,0.5,0.5,0.5)))
+  
 #TempExp
   data.TempExp <- list(y=y, beta.m=as.vector(c(0,0,0)), beta.v=solve(diag(1E-03,3)), Temp=Temp, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001)
   variable.names.TempExp <- c("tau_add", "beta")
   variable.namesout.TempExp <- c("tau_add", "beta", "mu")
-
+  init.TempExp <- list(list(tau_add=0.001, beta=c(-0.5,-0.5,-0.5)), list(tau_add=0.1, beta=c(0,0,0)), list(tau_add=1, beta=c(0.5,0.5,0.5)))
+  
 #Temp_Quad
   data.Temp_Quad <- list(y=y, beta.m=as.vector(c(0,0,0,0)), beta.v=solve(diag(1E-03,4)), Temp=Temp, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001)
   variable.names.Temp_Quad <- c("tau_add", "beta")
   variable.namesout.Temp_Quad <- c("tau_add", "beta", "mu")
-
+  init.Temp_Quad <- list(list(tau_add=0.001, beta=c(-0.5,-0.5,-0.5,-0.5)), list(tau_add=0.1, beta=c(0,0,0,0)), list(tau_add=1, beta=c(0.5,0.5,0.5,0.5)))
+  
 #Logistic
   data.Logistic <- list(y=y, beta.m=as.vector(c(0,0)), beta.v=solve(diag(1E-03,2)), N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001)
   variable.names.Logistic <- c("tau_add", "beta")
   variable.namesout.Logistic <- c("tau_add", "beta", "mu")
-
+  init.Logistic <- list(list(tau_add=0.001, beta=c(-0.5,-0.5)), list(tau_add=0.1, beta=c(0,0)), list(tau_add=1, beta=c(0.5,0.5)))
+  
 #Exponential
   data.Exponential <- list(y=y, beta.m=0, beta.v=1E-03, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001)
   variable.names.Exponential <- c("tau_add", "beta")
   variable.namesout.Exponential <- c("tau_add", "beta", "mu")
-
+  init.Exponential <- list(list(tau_add=0.001, beta=-0.5), list(tau_add=0.1, beta=0), list(tau_add=1, beta=0.5))
+  
 #RandomYear
   data.RandomYear <- list(y=y, year_no=year_no, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001)
   variable.names.RandomYear <- c("tau_add", "tau_yr")
   variable.namesout.RandomYear <- c("tau_add", "tau_yr","yr","mu", "x")
-
+  init.RandomYear <- list(list(tau_add=0.001, tau_yr=0.001), list(tau_add=0.1, tau_yr=0.1), list(tau_add=1, tau_yr=1))
+  
 #RandomYearIntercept
   data.RandomYearIntercept <- list(y=y, year_no=year_no, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001, beta.m=0, beta.v=1E-03)
   variable.names.RandomYearIntercept <- c("tau_add", "tau_yr", "beta")
   variable.namesout.RandomYearIntercept <- c("tau_add", "tau_yr","yr","mu", "x", "beta")
+  init.RandomYearIntercept <- list(list(tau_add=0.001, tau_yr=0.001, beta=-0.5), list(tau_add=0.1, tau_yr=0.1, beta=0), list(tau_add=1, tau_yr=1, beta=0.5))
   
-  #data.ChangepointTempExp <- list(y=y, beta.m=beta.m, beta.v=beta.v, Temp=Temp, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001)
- # variable.names.ChangepointTempExp <- c("tau_add", "beta", "k")
- # variable.namesout.ChangepointTempExp <- c("tau_add", "beta", "k", "mu")
+#data.ChangepointTempExp <- list(y=y, beta.m=beta.m, beta.v=beta.v, Temp=Temp, N=length(y),x_ic=log(0.1),tau_ic = 100,a_add = 0.001,r_add = 0.001)
+# variable.names.ChangepointTempExp <- c("tau_add", "beta", "k")
+# variable.namesout.ChangepointTempExp <- c("tau_add", "beta", "k", "mu")
   
   data = eval(parse(text = paste0('data.', model_name)))
   variable.names = eval(parse(text = paste0('variable.names.', model_name)))
   variable.namesout = eval(parse(text = paste0('variable.namesout.', model_name)))
+  init = eval(parse(text = paste0('init.', model_name)))
   
-  
-  return(list(data.model = data, variable.names.model = variable.names, variable.namesout.model = variable.namesout)) 
+  return(list(data.model = data, variable.names.model = variable.names, variable.namesout.model = variable.namesout, init.model = init)) 
 }
-
-
 
 preds_plug_ins <- function(model_name){
   
